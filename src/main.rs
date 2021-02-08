5f3229e96ec3d459079d541dd331e8ab523b36d9
Travis Weir <tweirtx@gmail.com>
2021-02-08 16:42:51 -0600

    Create REST API

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::thread;
+use warp::Filter;
 
 mod dns;
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let dnsargs = dns::Opt {
         multicast_group: "239.255.70.77".parse().unwrap(),
         host: "0.0.0.0".parse().unwrap(),
@@ -14,4 +15,22 @@ fn main() {
     });
     println!("Online");
     // now that we're broadcasting, start a web server to receive API calls to start/stop streaming
+
+    let start = warp::path!("start" / String / String)
+        .map(|url, key| start(url, key));
+
+    let stop = warp::path("stop").map(|| stop());
+
+    warp::serve(start.or(stop))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+
+fn start(url: String, key: String) -> &'static str {
+    println!("{} key {}", url, key);
+    return "STARTING";
+}
+
+fn stop() -> &'static str {
+    return "STOPPING";
 }
