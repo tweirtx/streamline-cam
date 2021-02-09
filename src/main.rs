6b1aa90f4fd403051fc503243930d039ba3c0e86
Travis <tweirtx@gmail.com>
2021-02-09 11:11:13 -0600

    Got it fully working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::process::{Command, Child};
 use warp::Filter;
+use warp::path::FullPath;
 
 mod dns;
 
@@ -20,12 +21,13 @@ async fn main() {
     println!("Online");
     // now that we're broadcasting, start a web server to receive API calls to start/stop streaming
 
-    let start = warp::path!("start" / String)
-        .map(|url| unsafe { start(url) });
+    let start_proc = warp::path::full().map(|url:FullPath| unsafe { start(url.as_str().replace("/start/", "").to_string()) });
+    let starter = warp::path("start").and(start_proc);
+
 
     let stop = warp::path("stop").map(|| unsafe { stop() });
 
-    warp::serve(start.or(stop))
+    warp::serve(starter.or(stop))
         .run(([0, 0, 0, 0], 3030))
         .await;
 }
@@ -34,15 +36,17 @@ unsafe fn start(url: String) -> &'static str {
     println!("{}", url);
 
     //Twitch URL = rtmp://live.twitch.tv/app/$KEY
-    let cmd: &str = &*format!("rtspsrc location=rtsp://localhost:1181/stream latency=100 ! rtph264depay ! queue ! flvmux ! rtmpsink location=\"{} live=1\"", url);
-    DISCRETE = Some(Command::new("gst-launch-1.0").arg(&cmd).spawn().expect("FAILED TO START STREAM"));
+    // let cmd: &str = &*format!("rtspsrc location=rtsp://localhost:1181/stream latency=100 ! rtph264depay ! queue ! flvmux ! rtmpsink location=\"{} live=1\"", url);
+    let location = "location=".to_owned() + url.as_str() + "live=1";
+    let argsarr = ["rtspsrc", "location=rtsp://localhost:1181/stream", "latency=100", "!", "rtph264depay", "!", "queue", "!", "flvmux", "!", "rtmpsink", &location];
+    DISCRETE = Some(Command::new("gst-launch-1.0").args(argsarr.iter()).spawn().expect("FAILED TO START STREAM"));
     return "STARTING";
 }
 
 unsafe fn stop() -> &'static str {
     let process = DISCRETE.take();
     if process.is_some() {
-        process.unwrap().kill();
+        process.unwrap().kill().expect("Error killing process");
     }
     return "STOPPING";
 }
